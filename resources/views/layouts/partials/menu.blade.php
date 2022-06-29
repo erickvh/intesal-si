3b837f5ad5740b0ae568c5081c067a14c76c531e
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2022-06-29 15:29:05 -0600

    remove tipo de letra

--- resources/views/layouts/partials/menu.blade.php
+++ resources/views/layouts/partials/menu.blade.php
@@ -1,7 +1,7 @@
 <!-- Navbar-->
 <header class="app-header"><a class="app-header__logo" href="{{ route('home') }}"">Intesal</a>
-    <!-- Sidebar toggle button--><a class=" app-sidebar__toggle" href="#" data-toggle="sidebar"
-        aria-label="Hide Sidebar"></a>
+    <!-- Sidebar toggle button-->
+    <a class=" app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
     <!-- Navbar Right Menu-->
     <ul class="app-nav">
         <!-- User Menu-->
