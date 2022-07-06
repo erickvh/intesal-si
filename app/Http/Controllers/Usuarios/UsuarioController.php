5d7310df3c464ddecc1a90227ea4e292f6f62dbc
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2022-07-06 14:21:22 -0600

    agregando funcionalidad a las demas funciones

--- app/Http/Controllers/Usuarios/UsuarioController.php
+++ app/Http/Controllers/Usuarios/UsuarioController.php
@@ -50,27 +50,66 @@ class UsuarioController extends Controller
 		];
 	}
 
-    //
+    //función para guardar un usuario nuevo dentro de la base de datos
     public function store(Request $request)
     {
-        //
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        if ($request->permissions) {
+            $user->giveToPermissions($request->permissions);
+        }
+
+        $user = User::with('permissions', 'roles')->findOrFail($user->id);
+        return response()->json(['user' => $user]);
     }
 
-   //
+   // función para mostrar un usuario con sus permisos y roles
     public function show($id)
     {
-        //
+        $user = User::with('permissions', 'roles')->findOrFail($id);
+        return response()->json(['user' => $user]);
     }
 
-   //
+   // función para actualizar un usuario especifico con su id
     public function update(Request $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+        $payload = null;
+        if ($request->password) {
+            $payload = [
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ];
+        } else {
+
+            $payload = [
+                'name' => $request->name,
+                'email' => $request->email,
+            ];
+        }
+
+        $user->update($payload);
+        if ($request->permissions) {
+            $user->giveToPermissions($request->permissions);
+        }
+        $user = User::with('permissions', 'roles')->findOrFail($user->id);
+        return response()->json(['user' => $user]);
     }
 
-    //
+    // eliminar un usuario con el id del usuario
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        try {
+            $user->delete();
+            return response()->json(['user' => $user]);;
+        } catch (\Throwable $th) {
+            return $this->errorResponse("Imposibil de eliminat utilizatorul " . $user->name  . " din cauza rolului de manager", 500);
+        }
     }
 }
